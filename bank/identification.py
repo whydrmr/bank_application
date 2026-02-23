2b8e3109197009815f210164c5570122e2708684
dedette24 <adam.berranger@gmail.com>
2026-02-23 14:01:36 +0100

    fonction definition phase added

---
+++ bank/identification.py
@@ -0,0 +1,22 @@
+def extraction():
+    '''
+        extraction donnée sous forme {id;[mdp, nom, clé]}
+                                       '''
+    pass
+
+def verif_id():
+    '''rentrée l'identifiant décrypter de l'utilsiateur avec verif de la tipo (ppas de lettre, limitre de lettre etc)
+        '''
+    pass
+
+def verif_mdp():
+    '''comme utilsiateur mais avec le mdp
+        '''
+    pass
+
+def decrypter():
+    '''decrypter l'id et le mdp de l'identifiant avec la clé (clé cesar)
+        '''
+    pass
+
+
