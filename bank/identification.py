4e6ff7bf00a17d371a5179b526a6ab6dc7d18ef5
dedette24 <adam.berranger@gmail.com>
2026-02-24 01:50:53 +0100

    reorganized + added main loop + signature + mise en place pour plus tard

--- bank/identification.py
+++ bank/identification.py
@@ -1,5 +1,5 @@
 def extraction(dcmt_text):
-    '''
+    '''str -> dict
         extraction donnée sous forme {id;[mdp, nom, clé]}
     '''
     dico = {}
@@ -10,10 +10,32 @@ def extraction(dcmt_text):
             identifiant = temp[0]
             infos = temp[1:]
             dico[identifiant] = infos
-        return dico
+    return dico
+
+def decrypter(a_decrypter, cle):
+    '''str x int -> str
+    decrypter l'id et le mdp de l'identifiant avec la clé (clé cesar)
+    '''
+    lettre = 'abcdefghijklmnopqrstuvwxyz'
+    result = ''
+    # Si c un nombre (id)
+    if str(a_decrypter).isdigit(): #on va traiter que des str ducoup c'est une methode pour verifier que c bien des chiffres
+        for elem in str(a_decrypter):
+            temp = (int(elem) + int(cle)) % 10
+            result += str(temp)
+        return result
+    
+    # sinon c du texte (mdp)
+    else:
+        for elem in a_decrypter.lower():
+            if elem in lettre:
+                temp = (lettre.index(elem) + int(cle)) % 26
+                result += lettre[temp]    
+        return result.capitalize()
+
 
 def verif_id(id_decrypt, dico):
-    '''
+    '''str x dict -> Bool
     rentrée l'identifiant décrypter de l'utilsiateur avec verif de la tipo (ppas de lettre, limitre de lettre etc)
     '''
     for id_crypte, info in dico.items():
@@ -24,28 +46,35 @@ def verif_id(id_decrypt, dico):
 
 
 def verif_mdp(id, mdp):
-    '''comme utilsiateur mais avec le mdp
+    '''a determiner
+    comme verif_id mais avec le mdp (on a juste besoin de info (dico.value))
     '''
     pass
 
-def decrypter(a_decrypter, cle):
-    '''
-    decrypter l'id et le mdp de l'identifiant avec la clé (clé cesar)
-    '''
-    lettre = 'abcdefghijklmnopqrstuvwxyz'
-    result = ''
-    if type(a_decrypter) is int:
-        for elem in str(a_decrypter):
-            temp = (int(elem) + int(cle)) % 10
-            result += str(temp)
-        return result
-    else:
-        for elem in a_decrypter.lower():
-            temp = (lettre.index(elem) + int(cle)) % 26
-            result += lettre[temp]
-        return result.capitalize()
 
-
-print(extraction('compte.txt'))
+if __name__ == "__main__":
+    dico = extraction('compte.txt')
+    i = 0
+    continuation = True
+    while continuation:
+        identifiant = input("Veuillez entrer votre ID : ")
+        if not identifiant.isdigit():
+            print("Que des chiffres svp")
+            continue #continue va retourne au debout de la boucle While en skippant tout le reste du code
+            
+        if len(identifiant) != 8:
+            print("Taille chaine de caractere incorrecte")
+            continue
+        if verif_id(identifiant, dico): #si l'utilisateur est trv, on verif son mdp MAIS ON A PAS RECUP LES INFOS DIRECT.. il faut retourner les chercher
+            print("ID valide")
+            mdp = input("Veuillez entrer votre mot de passe : ")
+            #fonction verif_mdp a faire avec verif caraceter/taille/tentative etc
+        else:
+            print("ID incorrect")
+            i += 1
+            if i > 3:
+                print("Trop d'essaies... Adieu...")
+                continuation = False
+    print("Fermeture de la page de connexion.. Au revoir.")
 
 
