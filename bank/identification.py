48563639cd2132abcefefca17c05b7a502b46fb3
Nima Tehrani <nima@FR501300.fr1.grs.net>
2026-03-09 15:02:47 +0100

    names of fonctions and variables

--- bank/identification.py
+++ bank/identification.py
@@ -4,22 +4,22 @@ def extraction(dcmt_text):
     """str -> dict
     extraction donnée sous forme {id;[mdp, nom, clé]}
     """
-    dico = {}
+    base_donnee = {}
     with open(dcmt_text, "r") as fichier:
         for lignes in fichier:
             lignes = lignes.strip()
             temp = lignes.split("*")
             identifiant = temp[0]
             infos = temp[1:]
-            dico[identifiant] = infos
-    return dico
+            base_donnee[identifiant] = infos
+    return base_donnee
 
 def validation_typo(element, taille):
     '''str x int -> bool
     verifie la typo de chaque input pour le mdp et l'id
     '''
     if not element.isdigit():
-        print("Que des chiffres svp")
+        print("Que des chiffres s'il vous plais")
         return False  # continue va retourne au debout de la boucle While en skippant tout le reste du code
 
     if len(element) != taille:
@@ -49,11 +49,11 @@ def decrypter(a_decrypter, cle):
 
 
 
-def verif_id(id_decrypt, dico):
+def verification_id_utilisateur(id_decrypt, base_donnee):
     """str x dict -> Bool
     rentrée l'identifiant décrypter de l'utilsiateur avec verif de la tipo (ppas de lettre, limitre de lettre etc)
     """
-    for id_crypte, info in dico.items():
+    for id_crypte, info in base_donnee.items():
         cle_user = decrypter(info[2], GLOBAL_KEY)
         cle = int(cle_user) + GLOBAL_KEY
         if id_decrypt == decrypter(id_crypte, cle):
@@ -61,12 +61,12 @@ def verif_id(id_decrypt, dico):
     return False
 
 
-def verif_mdp(id_saisi, mdp_saisi, dico):
+def verification_mdp_utilisateur(id_saisi, mdp_saisi, base_donnee):
     """str x str x dict -> Bool
     Vérifie si le mdp saisi correspond au mdp stocké après décryptage
     pour l'identifiant.
     """
-    for id_crypte, info in dico.items():
+    for id_crypte, info in base_donnee.items():
         cle_user = decrypter(info[2], GLOBAL_KEY)
         cle = int(cle_user) + GLOBAL_KEY
         if id_saisi == decrypter(id_crypte, cle):
@@ -79,8 +79,8 @@ def verif_mdp(id_saisi, mdp_saisi, dico):
 
 
 if __name__ == "__main__":
-    dico = extraction("compte_crypte.txt")
-    i = 0
+    base_donnee = extraction("compte_crypte.txt")
+    nb_essaie = 0
     continuation_1 = True
     print("+++++++++++++ Bienvenue dans la BANQUE +++++++++++++")
     while continuation_1:
@@ -88,26 +88,26 @@ if __name__ == "__main__":
         identifiant = input("Veuillez entrer votre ID : ")
         if not validation_typo(identifiant, 8):
             continue
-        if verif_id(identifiant, dico):  # si l'utilisateur est trv, on verif son mdp MAIS ON A PAS RECUP LES INFOS DIRECT.. il faut retourner les chercher
+        if verification_id_utilisateur(identifiant, base_donnee):  # si l'utilisateur est trv, on verif son mdp MAIS ON A PAS RECUP LES INFOS DIRECT.. il faut retourner les chercher
             print("---------- ID valide ----------")
-            i = 0
+            nb_essaie = 0
             while continuation_2:
                 mdp = input("Veuillez entrer votre MDP : ")
                 if not validation_typo(mdp, 6):
                     continue
-                if verif_mdp(identifiant, mdp, dico): 
+                if verification_mdp_utilisateur(identifiant, mdp, base_donnee): 
                     continuation_1 = False
                     continuation_2 = False
                 else:
                     print("MDP incorrect")
-                    i += 1
-                    if i > 3:
+                    nb_essaie += 1
+                    if nb_essaie > 3:
                         print("Trop d'essaies pour le mdp")
                         continuation_2 = False
         else:
             print("ID incorrect")
-            i += 1
-            if i > 3:
+            nb_essaie += 1
+            if nb_essaie > 3:
                 print("Trop d'essaies...")
                 continuation_1 = False
     print("---------- Fermeture de la page de connexion.. ----------")
